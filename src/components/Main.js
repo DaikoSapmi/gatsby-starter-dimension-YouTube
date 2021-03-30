d047b6cdb6aab16d1b85aad537ff40408de7dc59
RuneFjellheim <rune@fjellheim.tv>
2021-03-30 19:37:25 +0200

    Hjelp funker dett da? Form fix ørten

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,28 +10,6 @@ function encode(data) {
     .join('&')
 }
 
-export default function Contact() {
-  const [state, setState] = React.useState({})
-
-  const handleChange = (e) => {setState({ ...state, [e.target.name]: e.target.value })}
-}
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
-
-
 class Main extends React.Component {
   render() {
     let close = (
@@ -42,6 +20,29 @@ class Main extends React.Component {
         }}
       ></div>
     )
+
+    function Contact() {
+      const [state, setState] = React.useState({})
+    
+      const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+      }
+    
+      const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: encode({
+            'form-name': form.getAttribute('name'),
+            ...state,
+          }),
+        })
+          .then(() => navigate(form.getAttribute('action')))
+          .catch((error) => alert(error))
+      }
+    
 
     return (
       <div
@@ -208,6 +209,7 @@ class Main extends React.Component {
         </article>
       </div>
     )
+        }
   }
 }
 
