109bc5f5876efadc5fb60e9483ea4ed467ceb709
Rune Fjellheim <67677321+DaikoSapmi@users.noreply.github.com>
2021-03-31 10:03:16 +0200

    Tilbake til Ilen kirke

--- src/pages/index.js
+++ src/pages/index.js
@@ -137,7 +137,7 @@ class IndexPage extends React.Component {
           <div className="video-background">
         <div className="video-foreground">
           <YouTube
-            videoId="qHM1pZ15_ZI"
+            videoId="5dfaitOxqDU"
             opts={videoOptions}
             className="video-iframe"
             onReady={this._onReady}
