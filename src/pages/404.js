122b55972af0a5ee81573625d6b1b5a901e0ee38
DaikoSapmi <rune@fjellheim.tv>
2023-12-26 17:44:30 +0100

    Blog and posts.json

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,12 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Ii gávdno...</h1>
+    <p>Don deivvet justa dat siidu mii ii gávdno... :-/</p>
+    <h1>Finnes ikke...</h1>
+    <p>Du traff på en side som ikke finnes... :-/</p>
+    <h1>Not found...</h1>
+    <p>You just hit a route that doesn&#39;t exist... :-/</p>
   </Layout>
 )
 
---
+++ src/pages/blog.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Assuming the name of your JSON file is posts.json
+import postsData from '../static/posts.json';
+
+const Blog = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPost, setCurrentPost] = useState(null);
+
+  useEffect(() => {
+    // Load your posts data here
+    setPosts(postsData);
+    // Set the first post as the current post
+    if(postsData.length > 0) {
+      setCurrentPost(postsData[0]);
+    }
+  }, []);
+
+  const goToPost = (postId) => {
+    const post = posts.find(post => post.id === postId);
+    if(post) {
+      setCurrentPost(post);
+    }
+  }
+
+  if(!currentPost) return <p>Loading...</p>;
+
+  return (
+    <Layout>
+      <h1>{currentPost.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: currentPost.content }} />
+      {currentPost.previousId && (
+        <Link to="#" onClick={() => goToPost(currentPost.previousId)}>Previous Post</Link>
+      )}
+      {currentPost.nextId && (
+        <Link to="#" onClick={() => goToPost(currentPost.nextId)}>Next Post</Link>
+      )}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
+
+export default Blog
