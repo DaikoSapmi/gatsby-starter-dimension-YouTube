b7b50718e34f4dad72cb7e9bfa8dbd28dacc732c
DaikoSapmi <rune@fjellheim.tv>
2023-12-26 18:37:44 +0100

    Fixed some layout issues. Titles instead of next and previous.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
-import pic01 from '../images/pic01logo.png'
-
-
-// Assuming the name of your JSON file is posts.json
-import postsData from '../images/posts.json';
+import pic01 from '../path-to-your-image/pic01.jpg';
+import postsData from './images/posts.json';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
   const [currentPost, setCurrentPost] = useState(null);
 
   useEffect(() => {
-    // Load your posts data here
     setPosts(postsData);
-    // Set the first post as the current post
     if(postsData.length > 0) {
       setCurrentPost(postsData[0]);
     }
@@ -29,22 +24,32 @@ const Blog = () => {
 
   if(!currentPost) return <p>Loading...</p>;
 
+  const findPostTitleById = (postId) => {
+    const post = posts.find(post => post.id === postId);
+    return post ? post.title : '';
+  }
+
   return (
     <Layout>
-    <div className="logo">
-    <span className="image main">
-            <img src={pic01} alt="Logo Rune Fjellheim AS" style={{ width: '60%' }}/>
-          </span>
-    </div>
+      <div className="logo">
+        <span className="image main">
+          <img src={pic01} alt="Logo Rune Fjellheim AS" style={{ width: '60%' }}/>
+        </span>
+      </div>
       <Link to="/">Go back to the homepage</Link>
       <h1>{currentPost.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: currentPost.content }} />
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
+
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', marginRight:'50px' }}>
         {currentPost.previousId && (
-          <Link to="#" onClick={() => goToPost(currentPost.previousId)} style={{ float: 'left' }}>Previous Post</Link>
+          <Link to="#" onClick={() => goToPost(currentPost.previousId)} style={{ float: 'left' }}>
+            {findPostTitleById(currentPost.previousId)}
+          </Link>
         )}
         {currentPost.nextId && (
-          <Link to="#" onClick={() => goToPost(currentPost.nextId)} style={{ float: 'right' }}>Next Post</Link>
+          <Link to="#" onClick={() => goToPost(currentPost.nextId)} style={{ float: 'right' }}>
+            {findPostTitleById(currentPost.nextId)}
+          </Link>
         )}
       </div>
     </Layout>
