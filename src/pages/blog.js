314672a0ae057a3a5c57640a5aa3a632bb6e5ed4
DaikoSapmi <rune@fjellheim.tv>
2023-12-27 08:13:22 +0100

    Footer and wrapper

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import pic01 from '../images/pic01logo.png';
 import postsData from '../images/posts.json';
+import Footer from '../components/Footer'
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
@@ -31,25 +32,30 @@ const Blog = () => {
 
   return (
     <Layout>
-      <div className="logo">
-        <span className="image main">
-            <Link to="/"><img src={pic01} alt="Logo Rune Fjellheim AS" style={{ width: '10%' }}/></Link>
-        </span>
-      </div>
-      <h1>{currentPost.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: currentPost.content }} />
+        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+          <div id="wrapper">
+            <div className="logo">
+              <span className="image main">
+                  <Link to="/"><img src={pic01} alt="Logo Rune Fjellheim AS" style={{ width: '10%' }}/></Link>
+              </span>
+            </div>
+            <h1>{currentPost.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: currentPost.content }} />
 
-      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', marginRight:'70px' }}>
-        {currentPost.previousId && (
-          <Link to="#" onClick={() => goToPost(currentPost.previousId)} style={{ float: 'left' }}>
-            {findPostTitleById(currentPost.previousId)}
-          </Link>
-        )}
-        {currentPost.nextId && (
-          <Link to="#" onClick={() => goToPost(currentPost.nextId)} style={{ float: 'right' }}>
-            {findPostTitleById(currentPost.nextId)}
-          </Link>
-        )}
+            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', marginRight:'70px' }}>
+              {currentPost.previousId && (
+                <Link to="#" onClick={() => goToPost(currentPost.previousId)} style={{ float: 'left' }}>
+                  {findPostTitleById(currentPost.previousId)}
+                </Link>
+              )}
+              {currentPost.nextId && (
+                <Link to="#" onClick={() => goToPost(currentPost.nextId)} style={{ float: 'right' }}>
+                  {findPostTitleById(currentPost.nextId)}
+                </Link>
+              )}
+            </div>
+          <Footer timeout={this.state.timeout} />
+        </div>
       </div>
     </Layout>
   )
