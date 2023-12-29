48a7dea777132b60bd09b2159e97c826cfbee9b6
DaikoSapmi <rune@fjellheim.tv>
2023-12-29 23:33:37 +0100

    Test2

--- src/components/Main.js
+++ src/components/Main.js
@@ -46,8 +46,8 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">En ledende ekspert</h2>
-          <h2 className="major"> på samiske og arktiske spørsmål</h2>
+          <h2 className="major">En ledende ekspert på</h2>
+          <h2 className="major">samiske og arktiske spørsmål</h2>
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
@@ -66,8 +66,8 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">A Pioneering Expert in</h2>
-          <h2 className="major">Arctic and Indigenous Peoples' Issues</h2>
+          <h2 className="major">A Pioneering Expert in Arctic</h2>
+          <h2 className="major">and Indigenous Peoples' Issues</h2>
           <span className="image main">
             <img src={pic02} alt="" />
           </span>
