2ba014d02d9182ee8608272ac5ce0bc4bfa05107
DaikoSapmi <rune@fjellheim.tv>
2024-01-09 18:29:01 +0100

    Updated text!

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,13 +29,16 @@ class Main extends React.Component {
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Mu birra</h2>
+          <h2 className="major">Sámi ja Árktalaš</h2>
+          <h2 className="major">áššiid áššedovdi</h2>
           <span className="image main">
-            <img src={pic02} alt="Rune Fjellheim, Beaivválaš jođiheadji, Daglig leder, CEO" style={{ width: '60%' }} />
+            <img src={pic02} alt="Rune Fjellheim, Beaivválaš jođiheadji" style={{ width: '70%' }} />
           </span>
-          <p>Sápmelažžan bajásšadden Sámi servodagas, lean beroštuvvan Sámi-, Árktalaš- ja álgoálbmotáššiid juo studeantan. Bargoeallimis lean eanaš bargan politihka hábmemiin ja ovddideamiin oassin Norggabeal Sámedikki hálddáhusa, sorjjasmeahttun konsuleantan Jarumas AS:s, Sámeráđi árktalaš ja birasossodaga jođiheaddjin, Árktalaš ráđi álgoálbmogiid čállingotti jođiheaddjin ja 12 jagi Norggabeal Sámedikki direktevrran.</p>
-          <p>Dál mun doaimmahan iežan konsuleantafitnodaga, Rune Fjellheim AS. Buot dát posišuvnnat leat addán munnje vuđolaš ipmárdusa Sámi ja Árktisa gažaldagain iešguđet geahččanguovllus.</p>
-          <p>Spesialitehtat: Árktalaš politihkkagažaldagat, álgoálbmotpolitihkkaovddideapmi, máŋggabealát šiehtadallamat, Dálkkádatrievdangažaldagat ja Sámi gažaldagat. Logaldallamiid ja ovdanbuktimiid lea vejolaš diŋgut sihke digitálalaččat ja persovnnalaččat. <a href="/RuneFjellheim.vcf" download>Visitkoarta</a></p>
+          <p>Bures boahtin mu konsuleantafitnodahkii Rune Fjellheim AS. Mun lean oahppan sosiálaekonoma, Cand. Oecon, Oslo universitehtas ja mus lea lassioahppu sámegielas, informatihkas ja neahttavuđot mediain ja neahttabálvalusain.</p>
+          <p>Čiekŋalis ruohttasat sámi kultuvrras ja viiddis bargovásáhus lea addán munnje nanu máhtu sámi ja árktalaš gažaldagaid birra. Go lean bajásšaddan sámi birrasis, de lean bidjan ealliman min álbmoga beroštumiid ja vuoigatvuođaid ovddideapmái ja suodjaleapmái. Mus lea badjel moaddelogi jagi bargovásáhus, ja dasa gullet njunuš doaimmat Norgga Sámedikki direktevran, ja maiddái dehálaš doaimmat Sámiráđis ja Árktalaš Ráđi Álgoálbmotčállingottis.</p>
+          <p>Politihka ovddideapmi ja kultuvrraidgaskasaš áddejupmi leat báidnán mu karriera. Iežan rollaid bokte lean háhkan čiekŋalis máhtu ja áddejumi daid kompleaksa hástalusaid birra mat servodagain Árktisas ja álgoálbmogiin miehtá máilmmi leat.</p>
+          <p>Mu fitnodat fállá proseassajođiheami, logaldallamiid,, vuoigatvuođačielggademiid ja rávvemiid sámi, árktalaš ja álgoálbmotáššiin. Mu lahkonanvuohki lea huksejuvvon vásáhus- ja čiekŋalis máhtu ala, ja áigumus lea hábmet ceavzilis ja vuoiggalaš čovdosiid boahttevaš buolvvaide.</p>
+          <p>Mus lea maid stuora beroštupmi teknologiijii ja innovašuvdnii. Áŋgiris astoáigeprográmmerarin lean mun ovddidan mearkkašahtti gálggaid digitála čovdosiid ja streamingbálvalusaid siskkobealde. Lean gieskat álgán dahkkon jierpmi geavahišgoahtit, erenoamážit sámi geavahusaid ektui. Dat čájeha ahte máhtán ovttastahttit árbevirolaš máhtu ođđa teknologiijain. Mun sáhtán maiddái fállat digitála čoahkkinčovdosiid ja rávdnjemiid dárbbu mielde, oktan teknihkalaš dulkončovdosin (Zooma bokte).</p> <p><a href="/RuneFjellheim.vcf" download>Visitkoarta</a></p>
           {close}
         </article>
 
@@ -49,13 +52,13 @@ class Main extends React.Component {
           <h2 className="major">Ekspert på samiske</h2>
           <h2 className="major">og arktiske spørsmål</h2>
           <span className="image main">
-            <img src={pic02} alt="Rune Fjellheim, Beaivválaš jođiheadji, Daglig leder, CEO" style={{ width: '60%' }} />
+            <img src={pic02} alt="Rune Fjellheim, Daglig leder" style={{ width: '70%' }} />
           </span>
-          <p>Med dype røtter i samisk kultur og en solid karrierebakgrunn har jeg utviklet en solid kunnskap om samiske og arktiske spørsmål. Oppvokst i et samisk miljø, har jeg viet mitt liv til å fremme og beskytte vårt folks interesser og rettigheter. Min profesjonelle reise spenner over flere tiår og inkluderer ledende roller som direktør for Sametinget i Norge, samt viktige posisjoner i Samerådet og Arktisk Råds Urfolkssekretariat.</p>
-          <p>Min karriere har vært preget av en dyp forpliktelse til politikkutvikling og tverrkulturell forståelse. Gjennom mine ulike roller har jeg opparbeidet en sjelden dybde av kunnskap og forståelse rundt de komplekse utfordringene som samfunn i Arktis og urfolk verden over står overfor.</p>
-          <p>I dag driver jeg mitt eget konsulentfirma, Rune Fjellheim AS, hvor jeg tilbyr ekspertise innen prosessledelse, rettighetsutredninger og rådgivning på samiske, arktiske og urfolksspørsmål. Min tilnærming kombinerer tradisjonell kunnskap med moderne innsikt, og jeg er dedikert til å skape bærekraftige og rettferdige løsninger for fremtidige generasjoner.</p>
-          <p>Teknologi og innovasjon er også sentrale interesseområder for meg. Som en entusiastisk hobbyprogrammerer har jeg utviklet betydelige ferdigheter innen digitale løsninger og streamingtjenester. Min nylige inntreden i kunstig intelligensfeltet som en superbruker, særlig med fokus på samiske anvendelser, understreker min evne til å integrere tradisjonell kunnskap med cutting-edge teknologi. Jeg kan for eksempel tilby digitale møteløsninger og streaming ved behov, inkludert teknisk opplegg for tolkning (via Zoom).</p>
-          <p>Spesialitet er prosessledelse, utredninger innenfor feltet samiske rettigheter, Arktis og urfolksspørsmål. Jeg tilbyr foredrag og konsultasjoner, både digitalt og personlig, hvor jeg deler min unike blanding av innsikt og erfaring for å belyse og adressere de presserende utfordringene våre samfunn står overfor. <a href="/RuneFjellheim.vcf" download>Visittkort</a></p>
+          <p>Velkommen til meg, og mitt konsulentselskap Rune Fjellheim AS. Jeg er utdannet sosialøkonom, Cand. Oecon, fra Universitetet i Oslo og har tilleggsutdanning i samisk språk, informatikk og nettbaserte media og nettjenester.</p>
+          <p>Med dype røtter i samisk kultur og en bred karrierebakgrunn har jeg utviklet solid kunnskap om samiske og arktiske spørsmål. Oppvokst i et samisk miljø, har jeg viet mitt liv til å fremme og beskytte vårt folks interesser og rettigheter. Min profesjonelle reise spenner over flere tiår og inkluderer ledende roller som direktør for Sametinget i Norge, samt viktige posisjoner i Samerådet og Arktisk Råds Urfolkssekretariat.</p>
+          <p>Min karriere har vært preget av politikkutvikling og tverrkulturell forståelse. Gjennom mine roller har jeg opparbeidet en dybde av kunnskap og forståelse rundt de komplekse utfordringene som samfunn i Arktis og urfolk verden over står overfor.</p>
+          <p>Mitt selskap tilbyr prosessledelse, foredrag, rettighetsutredninger og rådgivning i samiske, arktiske og urfolksspørsmål. Min tilnærming kombinerer erfaringskunnskap med dybdeinnsikt, og jeg er dedikert til å skape bærekraftige og rettferdige løsninger for fremtidige generasjoner.</p>
+          <p>Teknologi og innovasjon er også sentrale interesseområder for meg. Som en entusiastisk hobbyprogrammerer har jeg utviklet betydelige ferdigheter innen digitale løsninger og streamingtjenester. Min nylige inntreden i kunstig intelligensfeltet som superbruker, særlig med fokus på samiske anvendelser, understreker min evne til å integrere tradisjonell kunnskap med ny teknologi. Jeg kan også tilby digitale møteløsninger og streaming ved behov, inkludert teknisk opplegg for tolkning (via Zoom).</p> <p><a href="/RuneFjellheim.vcf" download>Visittkort</a></p>
           {close}
         </article>
 
@@ -69,13 +72,13 @@ class Main extends React.Component {
           <h2 className="major">Expert on Sámi</h2>
           <h2 className="major">and Arctic Issues</h2>
           <span className="image main">
-            <img src={pic02} alt="Rune Fjellheim, Beaivválaš jođiheadji, Daglig leder, CEO" style={{ width: '60%' }} />
+            <img src={pic02} alt="Rune Fjellheim, CEO" style={{ width: '70%' }} />
           </span>
-          <p>With deep roots in the Sámi community and a career spanning over three decades, I, Rune Fjellheim, offer unparalleled expertise in Arctic policy, indigenous peoples' rights, and environmental stewardship. As a native Sámi, my journey began in our community, where I developed a keen interest in Arctic and indigenous issues. This early passion translated into a distinguished career that includes significant roles such as Director General at the Sámi Parliament of Norway, Executive Secretary of the Arctic Council Indigenous Peoples’ Secretariat, and a leader in the Sámi Council.</p>
-          <p>My extensive experience in policy planning and development, particularly within the Sámi Parliament, has equipped me with a multi-dimensional understanding of Arctic issues. I’ve been at the forefront of pivotal policy developments, including landmark agreements and initiatives that have shaped indigenous rights and environmental policies at an international level.</p>
-          <p>As the head of Rune Fjellheim AS, my consultancy firm, I specialize in guiding complex processes to produce comprehensive reports, analyses, and advice on human rights, Sámi issues, and leadership in Arctic indigenous peoples' matters, including tackling climate change challenges. My specialties encompass Arctic policy issues, indigenous peoples' policy development, multilateral negotiations, and climate change solutions.</p>
-          <p>A unique aspect of my profile is my technological adeptness. As a hobby programmer, I have honed skills in streaming services, particularly during the pandemic, and have recently emerged as a super user in the AI domain, focusing on Sámi area applications. This technological fluency allows me to offer contemporary solutions and lectures in these evolving fields.</p>
-          <p>I am available for consultations, lectures, and presentations, both digitally and in person, bringing a blend of traditional knowledge and modern technological insight to address today's pressing Arctic and indigenous issues. <a href="/RuneFjellheim.vcf" download>Business Card</a></p>
+          <p>Welcome to me and my consulting company Rune Fjellheim AS. I have a degree in Economics, Cand. Oecon, from the University of Oslo and have additional education in Sami language, computer science and online media and online services.</p>
+          <p>With deep roots in Sámi culture and a broad career background, I have developed solid knowledge of Sámi and Arctic issues. Raised in a Sámi environment, I have dedicated my life to promoting and protecting the interests and rights of our people. My professional journey spans several decades and includes leading roles as director of the Sámi Parliament in Norway, as well as important positions in the Saami Council and the Arctic Council's Indigenous Secretariat.</p>
+          <p>My career has been characterized by policy development and cross-cultural understanding. Through my roles, I have built up a depth of knowledge and understanding around the complex challenges that communities in the Arctic and Indigenous Peoples worldwide face.</p>
+          <p>My company offers process management, lectures, rights investigations and consultancy in Sámi, Arctic and indigenous issues. My approach combines experiential knowledge with in-depth insight, and I am dedicated to creating sustainable and fair solutions for future generations.</p>
+          <p>Technology and innovation are also key areas of interest for me. As an enthusiastic hobbyist programmer, I have developed significant skills in digital solutions and streaming services. My recent entry into the artificial intelligence field as a superuser, particularly with a focus on Sami applications, underlines my ability to integrate traditional knowledge with new technology. I can also offer digital meeting solutions and streaming if necessary, including technical arrangements for interpretation (via Zoom).</p><p><a href="/RuneFjellheim.vcf" download>Business Card</a></p>
           {close}
         </article>
 
