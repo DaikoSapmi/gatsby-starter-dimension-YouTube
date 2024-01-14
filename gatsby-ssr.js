75f9be66a8cc376dfc268c21705964d9dd43f1fa
DaikoSapmi <rune@fjellheim.tv>
2024-01-14 10:14:17 +0100

    Google Analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react");
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="gtag-js"
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-JP9199NQ00"
+    />,
+    <script
+      key="gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JP9199NQ00');
+        `,
+      }}
+    />,
+  ]);
+};
